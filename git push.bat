e95b6cc654a74aa827b6f95fa802580dacf06664
080user080 <080user080@gmail.com>
2025-10-12 00:02:21 +0300

    auto push

--- git push.bat
+++ git push.bat
@@ -1,15 +1,6 @@
 @echo off
 cd /d "D:\TTS\styletts2-ukrainian"
-echo === Adding changes ===
 git add -A
-
-echo === Commit ===
-set /p msg=Enter commit message: 
-if "%msg%"=="" set msg=update
-git commit -m "%msg%"
-
-echo === Push to GitHub ===
+git commit -m "auto push"
 git push -u origin main
 
-echo === Done ===
-pause
